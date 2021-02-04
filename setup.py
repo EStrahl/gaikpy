ef1df5cd4a10d08224bf3c4bb15267593752cf04
“Erik <strahl@informatik.uni-hamburg.de>
2021-02-04 15:13:30 +0100

    vweaion information updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name='gaikpy',
-    version='0.3.4.0',
+    version='0.3.4.2',
     author='Erik Strahl',
     author_email='strahl@informatik.uni-hamburg.de',
     description='Calculates and visualises forward and (full-pose) inverse kinematic realised with a genetic algorithm (ga) for URDF models',
